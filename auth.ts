5fc6ab3edd92641739012f920d349743650ff644
Jederson Andre <andre_sjx@live.com>
2025-05-09 20:36:41 -0300

    fix: disable verifiaction typescript

--- auth.ts
+++ auth.ts
@@ -1,3 +1,5 @@
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { JWT } from "next-auth/jwt"
