502b05828008e4db1006e701fb37bf4d5bdcab7a
Jederson Andre <andre_sjx@live.com>
2025-05-12 21:30:36 -0300

    fix: alteração tempo de duração jwt

--- auth.ts
+++ auth.ts
@@ -85,7 +85,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     session: {
         strategy: "jwt",
-        maxAge: 30 * 24 * 60 * 60, // 30 dias
+        maxAge: 3600, // 1 hora
     },
     secret: process.env.AUTH_SECRET,
 })
