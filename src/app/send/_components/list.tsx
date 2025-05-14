8bbd3e0706c4b29a2fcce55892022f71b84e0cd9
Jederson Andre <andre_sjx@live.com>
2025-05-13 21:36:52 -0300

    fix: add suspense page send

--- src/app/send/_components/list.tsx
+++ src/app/send/_components/list.tsx
@@ -1,5 +1,4 @@
 "use client";
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   Card,
   CardContent,
--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -1,25 +1,20 @@
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
+import { Suspense } from "react";
 import List from "./_components/list";
 
 const Send = () => {
-
   return (
- 
-      <div className="flex gap-6 item pt-3.5 justify-center">
-        <main >
+    <div className="flex gap-6 item pt-3.5 justify-center">
+      <main>
+        <Suspense>
           <List />
-        </main>
-{/*
+        </Suspense>
+      </main>
+      {/*
         <div className="flex flex-col gap-4">
           <SettingSendMessages totalLeads={totalContacts} listLeadsProps={listLeads!} />
         </div>
-        */
-}
-      </div>
-   
-
+        */}
+    </div>
   );
 };
 
